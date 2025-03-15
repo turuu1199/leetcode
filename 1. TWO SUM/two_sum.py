8e996658f8ac03b06d0400414ba7100adfcca4c4
Turmandakh <85722584+turuu1199@users.noreply.github.com>
2025-03-16 00:56:46 +0800

    two sum resolve

--- 1. TWO SUM/two_sum.py
+++ 1. TWO SUM/two_sum.py
@@ -1,1 +1,53 @@
-print('hello world')
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        # count = 0
+        # for index, value in enumerate(input):
+        #     count += 1
+        #     print('index', index, 'value ', value)
+
+        #     ind = 0
+        #     for i in input:
+        #         ind += 1
+        #         sum = value + i
+        #         if sum == target:
+        #             return index, count + ind - 2
+
+        """My solve """
+        count = 0
+        index = 0
+        for j in nums:
+            count += 1
+
+            ind = 0
+            for i in nums[count:]:
+                ind += 1
+                sum = i + j
+                if sum == target:
+                    return index, count + ind - 1
+        index += 1
+            
+        """ Best practise """
+
+        # class Solution(object):
+        #     def twoSum(self, nums, target):
+        #         HashMap = {}
+        #         for i, num in enumerate(nums):
+        #             diff = target - num
+        #             if diff in HashMap:
+        #                 return [HashMap[diff], i]
+        #             HashMap[num] = i
+        #         return
+        
+input = [3,2,4]
+target = 6
+
+# result 0,3
+sol = Solution()
+print(sol.twoSum(input, target))
+
